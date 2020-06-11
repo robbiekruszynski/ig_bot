adc116a92be316cfb081254d8801d2f35a1b35b3
robbie <robbiekruszynski@gmail.com>
2020-06-11 07:51:46 -0700

    install instapy

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ driver.get('https://www.instagram.com/')
 
 wait.until(ec.element_to_be_clickable((By.NAME, "username"))).send_keys("mr.percys_palace")
 el = wait.until(ec.element_to_be_clickable((By.NAME, "password")))
-el.send_keys("password")
+el.send_keys("D3arpercy")
 el.send_keys(Keys.ENTER)
 
 sleep(5)

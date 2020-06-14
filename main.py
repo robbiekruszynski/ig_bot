ee8bed81f070079b78288da6716039532f2e7e20
robbie <robbiekruszynski@gmail.com>
2020-06-14 07:59:05 -0700

    make adjustments to session set do like / etc

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from instapy import InstaPy
 
+
+# amount_number = 500
+
 session = InstaPy(username="mr.percys_palace", password="")
 session.login()
-session.like_by_tags(["travel", "adventure", "europe", "jellycat"], amount=5)
-session.set_do_follow(True, percentage=50)
-session.set_do_comment(True, percentage=50)
+session.like_by_tags(["travel", "adventure", "travelblogger"], amount=5)
+session.set_do_follow(True, percentage=75)
+session.set_do_comment(True, percentage=75)
+# session.set_do_like(enabled=True, percentage=100)
 session.set_comments(["Nice photo!", "<3", "Beautiful :heart_eyes:", "Follow me back!"])
-session.set_relationship_bounds(enabled=True, max_followers=8500)
-
+session.set_relationship_bounds(enabled=True, max_followers=5000, max_following=3000, min_followers=30, min_following=30)
+# session.follow_user_followers(['travelawesome'], amount=amount_number, randomize=False, interact=True, sleep_delay=300)
 session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
 
 session.end()
+
+
+
+
